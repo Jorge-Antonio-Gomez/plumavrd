5c317abc7307d4c9fd1c81a204bc39fd425ba14d
Jorge-Torre <jorgeantoniodarkman@gmail.com>
2022-01-14 14:05:14 -0600

    Se ha añadido el slider con sus funcionalidades JS.

--- js/javascritpt.js
+++ js/javascritpt.js
@@ -70,3 +70,93 @@
         }
     });
 }
+
+// Para el slider de comunicados
+{
+    // Declaración de constantes iniciales y variables
+    const releasesContainer = document.getElementById('releases_container');
+    const releasesItems = document.querySelectorAll('.releases_item');
+    const releasesController = document.getElementById('releases_controller');
+    // VAR DEC: Para saber en dónde estoy
+    var a = 0;
+    var b = releasesContainer.clientWidth;
+    var actualPosition = 1;
+    var actualPositionIndex = actualPosition - 1;
+
+
+    // Para inyectar la cantidad de dots necesaria en el slider
+    for (let i = 0; i < releasesItems.length; i++) {
+        releasesController.innerHTML += `
+            <div class="releases_controller-item"></div>
+        `;
+    }
+
+    // Obtener dots del DOM y activar el primero
+    const releasesControllerItems = document.querySelectorAll('.releases_controller-item');
+    releasesControllerItems[0].classList.add("active_item");
+
+    // Para activar el dot al que se la hace clic
+    for (let i = 0; i < releasesControllerItems.length; i++) {
+        releasesControllerItems[i].addEventListener("click", function(){
+            // Variables temporales
+            let i1 = i + 1;
+            let i1Limit = releasesControllerItems.length;
+
+            // Retirar el estilo "active_item" a todos los dots
+            for (let i = 0; i < releasesItems.length; i++) {
+                releasesControllerItems[i].classList.remove("active_item");
+            }
+
+            // Añadir el estilo "active_item" al dot que se le dió click
+            releasesControllerItems[i].classList.add("active_item");
+
+            // Aplicar posición del scroll
+            let releasesContainerWidth = releasesContainer.clientWidth;
+            releasesContainer.scrollLeft = (i * releasesContainerWidth);
+
+            // Para determinar la posición actual
+            a = b * i;
+            actualPosition = parseInt((a + b) / b);
+            actualPositionIndex = actualPosition - 1;
+            // console.log("Posición actual: " + actualPosition);
+        });
+    }
+
+    // Para activar el siguiente dot de forma automática con temporizador
+    setInterval(function() {
+        // VAR DEC: Ancho del contenedor de los releases_item
+        let releasesContainerWidth = releasesContainer.clientWidth;
+        // VAR DEC: Ancho total del contenedor de los releases_item, menos item
+        let releasesContainerFullWidthLess1 = (releasesContainer.clientWidth * (releasesItems.length - 1));
+
+
+        // Aplicar posición del scroll en un elemento "item" a la derecha. Y regresar al inicio si estamos en el último elemento 
+        if (releasesContainer.scrollLeft >= releasesContainerFullWidthLess1) {
+            releasesContainer.scrollLeft = 0;
+
+            // Esta variable será usada para saber dónde estoy
+            a = 0;
+        }
+        else {
+            releasesContainer.scrollLeft += (releasesContainerWidth);
+
+            // Esta variable será usada para saber dónde estoy
+            a = releasesContainer.scrollLeft + b;
+        }       
+
+        // Para saber en dónde estoy
+            // DECLARED let a = releasesContainer.scrollLeft;
+            // DECLARED let b = releasesContainer.clientWidth;
+        actualPosition = parseInt((a + b) / b);
+        actualPositionIndex = actualPosition - 1;
+
+        // Retirar el estilo "active_item" a todos los dots
+        for (let i = 0; i < releasesItems.length; i++) {
+            releasesControllerItems[i].classList.remove("active_item");
+        }
+
+        // Para Añadir el estilo "active_item" al dot que está activo en pantalla
+        releasesControllerItems[actualPositionIndex].classList.add("active_item");
+
+    }, 10000);
+}
